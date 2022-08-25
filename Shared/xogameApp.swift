75a325404ca5ae13dd9a2bee009f2f9238ab3663
Tony Han <s3821179@rmit.edu.vn>
2022-08-25 11:36:16 +0700

    Initial Commit

---
+++ Shared/xogameApp.swift
@@ -0,0 +1,17 @@
+//
+//  xogameApp.swift
+//  Shared
+//
+//  Created by Sang Yeob Han  on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct xogameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
