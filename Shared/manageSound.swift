1b151ff12c1975e6bae55ebfbc7cdbca8e18513e
Tony Han <s3821179@rmit.edu.vn>
2022-08-25 14:31:59 +0700

    adding sound effect on clicking

---
+++ Shared/manageSound.swift
@@ -0,0 +1,57 @@
+//
+//  manageSound.swift
+//  xogame (iOS)
+//
+//  Created by Sang Yeob Han  on 25/08/2022.
+//
+
+import SwiftUI
+import AVKit
+
+
+class SoundManager{
+    //sigliten
+    static let instance = SoundManager();
+    
+    var player: AVAudioPlayer?
+    
+    enum soundOption:String{
+        case dingSound
+        case doorKnock
+        case Doorknock
+    }
+    
+    func playSound(sound: soundOption){
+        //set up player
+        guard let url = Bundle.main.url(forResource: sound.rawValue , withExtension: ".mp3")else{return}
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }catch let error{
+            print("error playing sound \(error.localizedDescription)")
+        }
+    }
+}
+
+
+struct manageSound: View {
+    
+    var body: some View {
+        VStack{
+            Button("1"){
+                SoundManager.instance.playSound(sound: .dingSound)
+            }
+            Button("2"){
+                SoundManager.instance.playSound(sound: .Doorknock)
+            }
+        }
+    }
+}
+
+struct manageSound_Previews: PreviewProvider {
+    static var previews: some View {
+        manageSound()
+    }
+}
+
