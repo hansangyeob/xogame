ec95909dc583b9011cac0e3dc89c01dd2edece23
Tony Han <s3821179@rmit.edu.vn>
2022-08-26 15:20:18 +0700

    adding timer

--- Shared/CountdownTimer.swift
+++ Shared/CountdownTimer.swift
@@ -4,20 +4,25 @@
 //
 //  Created by Sang Yeob Han  on 26/08/2022.
 //
+//www.youtube.com/watch?v=-Xy1jBgGn7E
 
 import SwiftUI
 
-let defaultTimeRemaining:CGFloat = 6
-let lineWidth:CGFloat = 20
-let radius:CGFloat = 10
+//let defaultTimeRemaining:CGFloat = 5
+//let lineWidth:CGFloat = 20
+//let radius:CGFloat = 10
+
 
 struct CountdownTimer: View {
-
+    
+    @State public var timeRemaining:CGFloat = defaultTimeRemaining
     @State private var isActive = false
-    @State private var timeRemaining:CGFloat = defaultTimeRemaining
+    
+
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
+            
         VStack(spacing:25){
             ZStack{
                 Circle()
@@ -25,7 +30,7 @@ struct CountdownTimer: View {
                     .frame(width: 50, height: 50)
                 Circle()
                     .trim(from: 0, to: 1 - ((defaultTimeRemaining-timeRemaining)/defaultTimeRemaining))
-                    .stroke(timeRemaining>4 ? Color.green:
+                    .stroke(timeRemaining > 4 ? Color.green:
                                 timeRemaining > 2 ? Color.yellow
                             :Color.red
                             ,style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
@@ -62,8 +67,12 @@ struct CountdownTimer: View {
                 defaultTimeRemaining
             }
         })
+        
 }
 }
+
+
+
 struct CountdownTimer_Previews: PreviewProvider {
     static var previews: some View {
         CountdownTimer()
---
+++ Shared/sounds.swift
@@ -0,0 +1,27 @@
+//
+//  soundManager.swift
+//  xogame (iOS)
+//
+//  Created by Sang Yeob Han  on 25/08/2022.
+//
+
+import SwiftUI
+
+class soundManager: ObservableObject{
+    
+
+}
+
+struct sounds:View{
+    
+    var body:some View{
+        VStack{
+            Button("1"){
+                
+            }
+            Button("2"){
+                
+            }
+        }
+    }
+}
